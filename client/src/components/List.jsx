1a8847ad1c21a44ccbd3088f32677b58a46ff23b
Liezel Manalo <liezelanne9@gmail.com>
2019-08-10 15:16:32 -0700

    init (revised)

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -6,59 +6,21 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo: '',
-      todos: ['eat', 'sleep', 'code'],
+      
     };
-    this.getTodos = this.getTodos.bind(this);
-    this.postTodo = this.postTodo.bind(this);
-    this.deleteTodo = this.deleteTodo.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.postTodo = this.postTodo.bind(this);
-  }
-
-  componentDidMount() {
-    this.getTodos();
-  }
-
-  getTodos() {
-  }
-
-  postTodo(todo) {
-  }
-
-  deleteTodo(index) {
-  }
-
-  handleChange(event) {
-    this.setState({
-      todo: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.postTodo(this.state.todo);
-    event.target.reset();
   }
 
   render() {
     return (
       <div>
         <h1>List of things to do</h1>
-        <form onSubmit={event => this.handleSubmit(event)}>
+        <form>
           <h4>New todo:</h4>
-          <input onChange={this.handleChange} />
+          <input/>
         </form>
         <h4>Current todos</h4>
         <div>
-          {this.state.todos.map((todo, index) => (
-            <ListEntry
-              key={index}
-              index={index}
-              name={todo}
-              deleteTodo={this.deleteTodo}
-            />
-          ))}
+          
         </div>
       </div>
     );
