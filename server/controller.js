d76f8329ae2d69b515b24f627da98dbca8f9dbae
Dennis <chatkhan@Chatkhans-MacBook-Air.local>
2019-08-10 15:58:48 -0700

    Adding out components

--- server/controller.js
+++ server/controller.js
@@ -1,1 +1,14 @@
 // Controller here
+const controller = {
+    get: (req, res) => {
+        res.status(200).send('Hello from GET');
+    },
+    post: (req, res) => {
+        res.status(201).send('Hello from POST');
+    },
+    delete: (req, res) => {
+        res.status(202).send('Hello from DELETE');
+    }
+}
+
+module.exports = controller;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
 // Express here
+const express = require('express');
+// body parser reads it as request object
+const bodyParser = require('body-parser');
+// lets us know what kind of request we are serving
+const morgan = require('morgan');
+const path = require('path');
+const router = require('./router');
+
+// Creates server (can be called anything like app)
+const server = express();
+const port = 3000;
+
+// console.logs request to server
+server.use(morgan('dev'));
+// parses request and response bodies (w/out it, we only get buffers)
+server.use(bodyParser.json());
+
+// Serves something and listens to any request that comes from index.html
+server.use('/', express.static(path.join(__dirname + '/../client/dist')));
+
+server.use('/api', router);
+
+server.listen(port, () => console.log(`Connected to port ${port}`));
